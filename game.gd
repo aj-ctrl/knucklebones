c198f59f95fa6d09cfa9ce10cb77998489e49e02
AJ Duerr <65051889+aj-ctrl@users.noreply.github.com>
2023-11-06 08:45:43 -0500

    v1.1
    mostly graphical changes to make everything much more readable

--- game.gd
+++ game.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
-@onready var nodeP1Board = $P1Board
-@onready var nodeP2Board = $P2Board
-@onready var turn_tattle = $TurnTattle
-@onready var roll_tattle = $RollTattle
+@onready var nodeP1Board = $Control/P1Board
+@onready var nodeP2Board = $Control/P2Board
+@onready var turn_tattle = $Control/TurnTattle
+@onready var roll_tattle = $Control/RollTattle
 
-@onready var p1_col_1_total = $P1Totals/Col1Total
-@onready var p1_col_2_total = $P1Totals/Col2Total
-@onready var p1_col_3_total = $P1Totals/Col3Total
+@onready var p1_col_1_total = $Control/P1Totals/Col1Total
+@onready var p1_col_2_total = $Control/P1Totals/Col2Total
+@onready var p1_col_3_total = $Control/P1Totals/Col3Total
 
-@onready var p2_col_1_total = $P2Totals/Col1Total
-@onready var p2_col_2_total = $P2Totals/Col2Total
-@onready var p2_col_3_total = $P2Totals/Col3Total
+@onready var p2_col_1_total = $Control/P2Totals/Col1Total
+@onready var p2_col_2_total = $Control/P2Totals/Col2Total
+@onready var p2_col_3_total = $Control/P2Totals/Col3Total
 
 #controls whos turn it is p1 or p2
 var p1turn = true
@@ -56,19 +56,25 @@ var p2slots = {
 	"9" : 0, 
 	}
 
+func _ready():
+	RenderingServer.set_default_clear_color('#000000')
+
 func _process(delta):
 	process_scores()
 	render_board()
 	check_gamestate()
+	if !gameActive:
+		if Input.is_action_just_pressed("ui_select"):
+			get_tree().reload_current_scene()
 	#turn logic
 	if gameActive:
 		if p1turn:
 			turn_tattle.text = "Player 1's Turn"
-			roll_tattle.position = Vector2i($P1Board.position.x - 100,$P1Board.position.y + 25)
+			roll_tattle.position = Vector2i($Control/P1Board.position.x - 100,$Control/P1Board.position.y + 25)
 			p1_turn()
 		else:
 			turn_tattle.text = "Player 2's Turn"
-			roll_tattle.position = Vector2i($P2Board.position.x - 100,$P2Board.position.y + 25)
+			roll_tattle.position = Vector2i($Control/P2Board.position.x - 100,$Control/P2Board.position.y + 225)
 			p2_turn()
 
 func check_gamestate():
@@ -77,8 +83,9 @@ func check_gamestate():
 
 func end_game():
 	gameActive = false
-	$RollTattle.hide()
-	$WinTattle.show()
+	$Control/RollTattle.hide()
+	$Control/TurnTattle.hide()
+	$Control/WinTattle.show()
 
 func process_scores():
 	process_score(p1col1, p1_col_1_total)
@@ -90,14 +97,14 @@ func process_scores():
 	var p1score = int(p1_col_1_total.text) + int(p1_col_2_total.text) + int(p1_col_3_total.text)
 	var p2score = int(p2_col_1_total.text) + int(p2_col_2_total.text) + int(p2_col_3_total.text)
 	if p1score > p2score:
-		$WinTattle.text = "Player 1 wins!"
+		$Control/WinTattle.text = "[center]Player 1 wins![/center]"
 	else:
 		if p2score > p1score:
-			$WinTattle.text = "Player 2 wins!"
+			$Control/WinTattle.text = "[center]Player 2 wins![/center]"
 		else:
-			$WinTattle.text = "Draw!"
-	$P1Totals/GrandTotal.text = "Score: " + str(p1score)
-	$P2Totals/GrandTotal.text = "Score: " + str(p2score)
+			$Control/WinTattle.text = "[center]Draw![/center]"
+	$Control/P1Totals/GrandTotal.text = "Score: " + str(p1score)
+	$Control/P2Totals/GrandTotal.text = "Score: " + str(p2score)
 
 func process_score(column:Array, totalDisplay:Object):
 	var total = 0
@@ -106,44 +113,30 @@ func process_score(column:Array, totalDisplay:Object):
 			total = column[0]
 		2:
 			if column[0] == column[1]:
-				total = column[0] * column[1]
+				total = (column[0] + column[1]) * 2
 			else:
 				total = column[0] + column[1]
 		3:
 			total = column[0] + column[1] + column[2]
 			if column[0] == column[1]:
-				total = column[0] * column[1] + column[2]
+				total = ((column[0] + column[1]) * 2) + column[2]
 			if column[0] == column[2]:
-				total = column[0] * column[2] + column[1]
+				total = ((column[0] + column[2]) * 2) + column[1]
 			if column[1] == column[2]:
-				total = column[1] * column[2] + column[0]
+				total = ((column[1] + column[2]) * 2) + column[0]
 			if column[0] == column[1] and column[0] == column[2]:
-				total = column[0] * column[1] * column[2]
-	totalDisplay.text = str(total)
+				total = (column[0] + column[1] + column[2]) * 3
+	totalDisplay.text = "[center]" + str(total) + "[/center]"
 
-func handle_pops(p1column, p2column):
-	var matchingVal = -1
-	var popMatchs = false
-	for die in p1column:
-		if p2column.has(die):
-			popMatchs = true
-			matchingVal = die
-	if popMatchs:
-		print("popping", p1column, p2column)
-		while p1column.has(matchingVal):
-			var toPop = p1column.find(matchingVal)
-			p1column.pop_at(toPop)
-		while p2column.has(matchingVal):
-			var toPop = p2column.find(matchingVal)
-			p2column.pop_at(toPop)
-		print("popped", p1column, p2column)
-		
-		popMatchs = false
+func handle_pops(currentDie:int, opponentColumn:Array):
+	while opponentColumn.has(currentDie):
+		var toPop = opponentColumn.find(currentDie)
+		opponentColumn.pop_at(toPop)
 
 func render_board():
 	update_slots()
-	nodeP1Board.text = "["+str(p1slots["1"])+"]" + "["+str(p1slots["4"])+"]" + "["+str(p1slots["7"])+"]" + "["+str(p1slots["2"])+"]" + "["+str(p1slots["5"])+"]" + "["+str(p1slots["8"])+"]" + "["+str(p1slots["3"])+"]" + "["+str(p1slots["6"])+"]" + "["+str(p1slots["9"])+"]"
-	nodeP2Board.text = "["+str(p2slots["3"])+"]" + "["+str(p2slots["6"])+"]" + "["+str(p2slots["9"])+"]" + "["+str(p2slots["2"])+"]" + "["+str(p2slots["5"])+"]" + "["+str(p2slots["8"])+"]" + "["+str(p2slots["1"])+"]" + "["+str(p2slots["4"])+"]" + "["+str(p2slots["7"])+"]"
+	nodeP1Board.text = "[center]"+"["+str(p1slots["1"])+"]" + "["+str(p1slots["4"])+"]" + "["+str(p1slots["7"])+"]" + "["+str(p1slots["2"])+"]" + "["+str(p1slots["5"])+"]" + "["+str(p1slots["8"])+"]" + "["+str(p1slots["3"])+"]" + "["+str(p1slots["6"])+"]" + "["+str(p1slots["9"])+"]"+"[/center]"
+	nodeP2Board.text = "[center]"+"["+str(p2slots["3"])+"]" + "["+str(p2slots["6"])+"]" + "["+str(p2slots["9"])+"]" + "["+str(p2slots["2"])+"]" + "["+str(p2slots["5"])+"]" + "["+str(p2slots["8"])+"]" + "["+str(p2slots["1"])+"]" + "["+str(p2slots["4"])+"]" + "["+str(p2slots["7"])+"]"+"[/center]"
 
 func p1_turn():
 	if p1turnInit:
@@ -153,21 +146,21 @@ func p1_turn():
 	
 	if Input.is_action_just_released("Player1_left"):
 		var turnTaken = add_roll_to_col(p1col1, currentRoll)
-		handle_pops(p1col1, p2col1)
+		handle_pops(currentRoll, p2col1)
 		if turnTaken:
 			p1turn = false
 			p1turnInit = true
 	
 	if Input.is_action_just_released("Player1_middle"):
 		var turnTaken = add_roll_to_col(p1col2, currentRoll)
-		handle_pops(p1col2, p2col2)
+		handle_pops(currentRoll, p2col2)
 		if turnTaken:
 			p1turn = false
 			p1turnInit = true
 	
 	if Input.is_action_just_released("Player1_right"):
 		var turnTaken = add_roll_to_col(p1col3, currentRoll)
-		handle_pops(p1col3, p2col3)
+		handle_pops(currentRoll, p2col3)
 		if turnTaken:
 			p1turn = false
 			p1turnInit = true
@@ -180,21 +173,21 @@ func p2_turn():
 	
 	if Input.is_action_just_released("Player2_left"):
 		var turnTaken = add_roll_to_col(p2col1, currentRoll)
-		handle_pops(p1col1, p2col1)
+		handle_pops(currentRoll, p1col1)
 		if turnTaken:
 			p1turn = true
 			p2turnInit = true
 	
 	if Input.is_action_just_released("Player2_middle"):
 		var turnTaken = add_roll_to_col(p2col2, currentRoll)
-		handle_pops(p1col2, p2col2)
+		handle_pops(currentRoll, p1col2)
 		if turnTaken:
 			p1turn = true
 			p2turnInit = true
 	
 	if Input.is_action_just_released("Player2_right"):
 		var turnTaken = add_roll_to_col(p2col3, currentRoll)
-		handle_pops(p1col3, p2col3)
+		handle_pops(currentRoll, p1col3)
 		if turnTaken:
 			p1turn = true
 			p2turnInit = true
